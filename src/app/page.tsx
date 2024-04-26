5b375f198a78869a0a11aaf50555d93252776bd2
Iliya Brook <iliyabrook1987@gmail.com>
2024-04-26 17:46:51 +0300

    route fix a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { VideoBackground } from '@/components/VideoBackground/VideoBackground'
 import { logVisits } from '@/utils/logVisits'
 import { ScrollBarWrapper } from '@/components/ScrollBarWrapper/ScrollBarWrapper'
 import { Metadata } from 'next'
+import { headers } from 'next/headers'
 
 export const metadata: Metadata = {
 	title: 'Home - Veronika Brook',
@@ -11,9 +12,16 @@ export const metadata: Metadata = {
 }
 
 export default async function Home() {
+	const headerList = headers()
+	const hostname = headerList.get('host')
+	console.log('Home app hostname:', hostname)
+
 	void logVisits('home')
 	return (
 		<ScrollBarWrapper>
+			<div style={{ display: 'flex', justifyContent: 'center' }}>
+				<h1>domain: {hostname}</h1>
+			</div>
 			<main className={styles.main}>
 				<div className={styles.videoContainer}>
 					<VideoBackground src="home_bg_video.mp4" type="video/mp4" />
