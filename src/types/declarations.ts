a8c35dde59785d0de3311efaa4931343bf961f2a
Iliya Brook <iliyabrook1987@gmail.com>
2024-04-27 14:52:26 +0300

    test with no db logic 2

--- src/types/declarations.ts
+++ src/types/declarations.ts
@@ -3,38 +3,27 @@
 import 'next-auth'
 
 declare module 'next-auth' {
-  interface User {
-    id: string
-    isAdmin?: boolean
-  }
+	interface User {
+		id: string
+		isAdmin?: boolean
+	}
 }
 
 declare module 'next-auth/jwt' {
-  interface JWT {
-    id?: string
-    isAdmin?: boolean
-  }
+	interface JWT {
+		id?: string
+		isAdmin?: boolean
+	}
 }
 
-declare module 'next-auth/react' {
-  interface Session {
-    user?: {
-      id?: string
-      name?: string | null
-      email?: string | null
-      image?: string | null
-      isAdmin?: boolean
-    }
-  }
-}
 declare module 'next-auth' {
-  interface Session {
-    user: {
-      id?: string
-      name?: string
-      email?: string
-      image?: string
-      isAdmin?: boolean
-    }
-  }
+	interface Session {
+		user: {
+			id?: string
+			name?: string
+			email?: string
+			image?: string
+			isAdmin?: boolean
+		}
+	}
 }
