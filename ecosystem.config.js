b5049239b608ce67244afcf37f11d1dcaa5b6313
Iliya Brook <iliyabrook1987@gmail.com>
2024-04-28 12:07:07 +0300

    aws amplify g2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,28 @@
 module.exports = {
 	apps: [
 		{
-			name: 'admin_production',
+			name: 'nika_brook',
 			script: 'npm',
 			args: 'start-prod',
 			cwd: '/var/www/nika-brook'
+		}
+	]
+}
+
+// second variant
+
+module.exports = {
+	apps: [
+		{
+			name: 'nika_brook',
+			script: 'npm',
+			args: 'run start-prod',
+			cwd: '/var/www/nika-brook',
+			watch: true,
+			restart_delay: 5000,
+			env: {
+				NODE_ENV: 'production'
+			}
 		}
 	]
 }
