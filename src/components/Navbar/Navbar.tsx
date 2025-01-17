fdb276007b5b7ceee139fd604a271a92463c0c53
Iliya Brook <iliyabrook1987@gmail.com>
2025-01-17 10:19:45 +0200

    nav bar refactor

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,82 +1,17 @@
 'use client'
-import NavBarLogo from '@/components/Navbar/components/navBarLogo/NavBarLogo'
 import SocialNetLinks from '@/components/Navbar/components/socialNetLinks/socialNetLinks'
-import { setActivePath } from '@/utils/setActivePath'
-import classNames from 'classnames'
+import getNavBarItems from '@/components/Navbar/navBarItems'
+import { usePathname, useRouter } from 'next/navigation'
 import { Menubar } from 'primereact/menubar'
-import { MenuItem, type MenuItemOptions } from 'primereact/menuitem'
 import React, { useMemo } from 'react'
 import styles from './Navbar.module.scss'
-import { usePathname, useRouter } from 'next/navigation'
 
 
 const Navbar = () => {
+	
 	const pathname = usePathname()
 	const router = useRouter()
-	
-	
-	const RouteTemplate = (item: MenuItem, options:MenuItemOptions) => {
-		const routeName = item.id;
-		return (
-			<a
-				className={classNames(
-					options.className,
-					setActivePath(pathname, routeName)
-				)}
-				onClick={() => router.push(routeName)}
-			>
-				{item.label}
-			</a>
-		);
-	}
-	
-	const navBarItems = useMemo(() => {
-		const navItems: MenuItem[] = [
-			{
-				label: 'Home',
-				id: '/',
-				template: RouteTemplate
-			},
-			{
-				label: 'Biography',
-				id: '/biography',
-				template: RouteTemplate
-			},
-			{
-				className: 'logo',
-				template: NavBarLogo
-			},
-			{
-				label: 'Media',
-				id: '/media',
-				className: classNames(
-					setActivePath(pathname, '/media/video'),
-					setActivePath(pathname, '/media/photo')
-				),
-				items: [
-					{
-						label: 'Video',
-						id: '/media/video',
-						className: 'sub-route-link',
-						template: RouteTemplate
-						
-					},
-					{
-						label: 'Photo',
-						id: '/media/photo',
-						className: 'sub-route-link photo-link',
-						template: RouteTemplate
-					}
-				]
-			},
-			{
-				label: 'Contact',
-				id: '/contact',
-				template: RouteTemplate
-			},
-		]
-		return navItems
-	}, [pathname])
+	const navBarItems = useMemo(() => getNavBarItems(pathname, router), [pathname])
 	
 	return (
 		<div className={styles.root}>
---
+++ src/components/Navbar/navBarItems.tsx
@@ -0,0 +1,70 @@
+import { setActivePath } from '@/utils/setActivePath'
+import classNames from 'classnames'
+import type { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime'
+import { MenuItem, type MenuItemOptions } from 'primereact/menuitem'
+import React from 'react'
+
+const RouteTemplate = (item: MenuItem, options:MenuItemOptions, pathname: string, router: AppRouterInstance) => {
+	const routeName = item.id;
+	return (
+		<a
+			className={classNames(
+				options.className,
+				setActivePath(pathname, routeName)
+			)}
+			onClick={() => router.push(routeName)}
+		>
+			{item.label}
+		</a>
+	);
+}
+
+const getNavBarItems = (pathname: string, router: AppRouterInstance): MenuItem[] => {
+	const template = (item: MenuItem, options: MenuItemOptions) => RouteTemplate(item, options, pathname, router)
+	return [
+		{
+			label: 'Home',
+			id: '/',
+			template
+		},
+		{
+			label: 'Biography',
+			id: '/biography',
+			template
+		},
+		{
+			className: 'logo',
+			template
+		},
+		{
+			label: 'Media',
+			id: '/media',
+			className: classNames(
+				setActivePath(pathname, '/media/video'),
+				setActivePath(pathname, '/media/photo')
+			),
+			items: [
+				{
+					label: 'Video',
+					id: '/media/video',
+					className: 'sub-route-link',
+					template
+					
+				},
+				{
+					label: 'Photo',
+					id: '/media/photo',
+					className: 'sub-route-link photo-link',
+					template
+				}
+			]
+		},
+		{
+			label: 'Contact',
+			id: '/contact',
+			template
+		},
+	]
+}
+
+export default getNavBarItems
