6d08f810884d94b7e99a7c19cccab321b33a2ce9
Iliya Brook <iliyabrook1987@gmail.com>
2025-01-25 14:19:58 +0200

    home responsive done

--- src/utils/enviroments.ts
+++ src/utils/enviroments.ts
@@ -1,10 +1,1 @@
 export const isDevelopment = process.env.NODE_ENV === 'development';
-export const getApiBaseUrl = (): string => {
-  if (typeof window === 'undefined') {
-    // server-side url
-    return process.env.API_URL_INTERNAL || process.env.NEXT_PUBLIC_API_URL
-  } else {
-    // client-side url
-    return process.env.NEXT_PUBLIC_API_URL || '/api'
-  }
-}
