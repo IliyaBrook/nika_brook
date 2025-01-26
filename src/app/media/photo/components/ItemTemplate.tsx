39e01f9ba03526c71de0529aac65934e6be1e5d7
Iliya Brook <iliyabrook1987@gmail.com>
2025-01-26 23:38:32 +0200

    unused packages

--- src/app/media/photo/components/ItemTemplate.tsx
+++ src/app/media/photo/components/ItemTemplate.tsx
@@ -3,61 +3,87 @@ import type { ImageItem } from '@/types/sharable.types.ts'
 import getElementsByXPath from '@/utils/getElementsByXPath'
 import classNames from 'classnames'
 import { Image as PrimeImage } from 'primereact/image'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 
-export const ItemTemplate = ({ isReady, index, ...item }: ImageItem) => {
-	const currentElementRendered = useRef(0)
+interface ItemTemplateProps extends ImageItem{
+	item: ImageItem
+	index: number
+	onLoad?: () => void
+}
+
+export function ItemTemplate({ index, onLoad, ...item }: ItemTemplateProps) {
+	const [loaded, setLoaded] = useState(false)
+	const primeImgRef = useRef<PrimeImage>(null)
+	const creditInserted = useRef(false)
 	
 	useEffect(() => {
-		if (isReady) {
-			const creditTo =
-				`
-				<div
-					class='mediaPhotoCreditTo--${item.id}'
-					style='color: ${item?.creditColor}'
-				>
-					${item?.credit ?? ''}
-				</div>
-			`
-			const carouselItems = getElementsByXPath({ xpath: `//div[contains(@class, 'p-carousel-item') and @aria-label='${index}']` })
+		const primeImageInstance = primeImgRef.current
+		if (!primeImageInstance) return
+		
+		const rawImageEl = primeImageInstance.getImage()
+		if (!rawImageEl) return
+		
+		const handleLoad = () => {
+			setLoaded(true)
+			onLoad?.()
+		}
+		
+		rawImageEl.addEventListener('load', handleLoad)
+		return () => {
+			rawImageEl.removeEventListener('load', handleLoad)
+		}
+	}, [primeImgRef.current])
+	
+	useEffect(() => {
+		if (loaded && !creditInserted.current) {
+			const creditTo = `
+        <div
+          class='mediaPhotoCreditTo--${item.id}'
+          style='color: ${item?.creditColor}'
+        >
+          ${item?.credit ?? ''}
+        </div>
+      `
+			
+			const carouselItems = getElementsByXPath({
+				xpath: `//div[contains(@class, 'p-carousel-item') and @aria-label='${index}']`
+			})
+			
 			if (carouselItems?.length > 0) {
 				const carouselItem = carouselItems[0]
-				if (carouselItem instanceof HTMLElement && currentElementRendered.current === 0) {
+				if (carouselItem instanceof HTMLElement) {
 					carouselItem.insertAdjacentHTML('beforeend', creditTo)
-					currentElementRendered.current++
 					carouselItem.style.position = 'relative'
+					creditInserted.current = true
 				}
 			}
 		}
-	}, [isReady])
+	}, [loaded, index, item])
 	
 	return (
 		<div className={classNames(styles.itemTemplate)}>
 			<div className={styles.thumbnailImage}>
 				<div className={styles.imageItemWrapper}>
-					<PrimeImage
-						src={item?.itemImageSrc}
-						alt={item?.alt}
-						preview
-						loading='eager'
-						className={`${styles.img} galleryImageItem`}
-						style={{visibility: isReady ? 'visible' : 'hidden'}}
-						
-					/>
-					{!isReady && (
-						<PrimeImage
-							src='/images/skeleton.svg'
-							alt='sekelton'
-							preview
-							loading='eager'
+					
+					{!loaded && (
+						<img
+							src="/images/skeleton.svg"
+							alt="Loading skeleton"
 							className={`${styles.img} ${styles.galleryImgSkeleton}`}
 						/>
 					)}
+					
+					<PrimeImage
+						ref={primeImgRef}
+						src={item.itemImageSrc}
+						alt={item.alt}
+						preview
+						className={`${styles.img} galleryImageItem`}
+						style={{ display: loaded ? 'block' : 'none' }}
+					/>
 				</div>
 			</div>
 		</div>
 	)
 }
-
-
--- src/app/media/photo/components/PhotoGalleria.tsx
+++ src/app/media/photo/components/PhotoGalleria.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { images } from '@/app/media/photo/data'
 import { isDevelopment } from '@/utils/enviroments'
-import getElementsByXPath from '@/utils/getElementsByXPath'
 import dynamic from 'next/dynamic'
 import { CarouselResponsiveOption } from 'primereact/carousel'
 import React, { useEffect, useRef, useState } from 'react'
@@ -13,7 +12,7 @@ const Carousel = dynamic(
 			({ Carousel }) => Carousel
 		),
 	{
-		ssr: false,
+		ssr: false
 	}
 )
 
@@ -23,112 +22,56 @@ const ItemTemplate = dynamic(
 			({ ItemTemplate }) => ItemTemplate
 		),
 	{
-		ssr: false,
+		ssr: false
 	}
 )
 
 export default function PhotoGalleria() {
 	const carouselRef = useRef<any>(null)
+	const [loadedCount, setLoadedCount] = useState(0)
+	const handleImageLoad = () => setLoadedCount(prev => prev + 1)
+	console.log('loadedCount:', loadedCount)
 	
-	const [isReady, setReady] = useState<boolean>(false)
 	const responsiveOptions: CarouselResponsiveOption[] = [
-		{
-			breakpoint: '1400px',
-			numVisible: 4,
-			numScroll: 1
-		},
-		{
-			breakpoint: '1199px',
-			numVisible: 3,
-			numScroll: 1
-		},
-		{
-			breakpoint: '767px',
-			numVisible: 2,
-			numScroll: 1
-		},
-		{
-			breakpoint: '575px',
-			numVisible: 1,
-			numScroll: 1
-		}
+		{ breakpoint: '1400px', numVisible: 4, numScroll: 1 },
+		{ breakpoint: '1199px', numVisible: 3, numScroll: 1 },
+		{ breakpoint: '767px', numVisible: 2, numScroll: 1 },
+		{ breakpoint: '575px', numVisible: 1, numScroll: 1 }
 	]
+	
 	useEffect(() => {
-		if (carouselRef?.current) {
-			carouselRef.current.stopAutoplay()
+		if (loadedCount === images.length) {
+			carouselRef.current?.startAutoplay()
 		}
-	}, [carouselRef?.current])
+	}, [loadedCount])
+	
 	useEffect(() => {
-		const observer = new MutationObserver(mutationsList => {
-			for (const mutation of mutationsList) {
-				if (mutation.type === 'attributes' && mutation.target instanceof Element) {
-					if (mutation.target?.tagName === 'IMG') {
-						const carouselItems = document.querySelectorAll('.p-carousel-item')
-						if (carouselItems.length > 0) {
-							setTimeout(() => {
-								setReady(true)
-							}, 1000)
-						}
-					}
-				}
-			}
-		})
-		if (observer) {
-			try {
-				observer.observe(document, { childList: true, subtree: true, attributes: true })
-			} catch (e) {
-				console.log("observer error :", e)
-			}
-		}
-		
-		return () => {
-			observer.disconnect()
-		}
+		carouselRef.current?.stopAutoplay()
 	}, [])
 	
-	useEffect(() => {
-		const imageButton = getElementsByXPath({xpath: "//div[@class='p-carousel-items-content']//button"})
-		if (imageButton && imageButton.length > 0 && isReady) {
-			carouselRef.current.startAutoplay()
-
-			const handleMouseEnter = () => {
-				carouselRef.current.stopAutoplay()
-			};
-
-			const handleMouseLeave = () => {
-				setTimeout(() => {
-					carouselRef.current.startAutoplay()
-				}, 1000)
-			};
-			for (const btn of imageButton) {
-				btn.addEventListener('mouseenter', handleMouseEnter);
-				btn.addEventListener('mouseleave', handleMouseLeave);
-			}
-
-			return () => {
-				for (const btn of imageButton) {
-					btn.removeEventListener('mouseenter', handleMouseEnter);
-					btn.removeEventListener('mouseleave', handleMouseLeave);
-				}
-			};
-		}
-	}, [isReady])
 	
-	
-	const autoplayInterval = !isDevelopment ? 3000 : undefined
+	// const autoplayInterval = !isDevelopment ? 3000 : undefined
+	const autoplayInterval = 3000
 	
 	return (
 		<div className={styles.carouselWrapper}>
+			
 			<Carousel
 				/* @ts-ignore */
 				ref={carouselRef}
 				value={images}
 				numVisible={3}
-				autoplayInterval={autoplayInterval}
 				orientation='horizontal'
 				verticalViewPortHeight='360px'
-				itemTemplate={(item) =>  <ItemTemplate {...item} isReady={isReady} />}
+				autoplayInterval={autoplayInterval}
 				responsiveOptions={responsiveOptions}
+				itemTemplate={(item) => (
+					<ItemTemplate
+						{...item}
+						index={images.indexOf(item)}
+						onLoad={handleImageLoad}
+					/>
+				)}
 			/>
 		</div>
 	)
