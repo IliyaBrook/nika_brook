55ef10360a44909fb7900e20ecf2d32bbed43281
Iliya Brook <iliyabrook1987@gmail.com>
2025-03-24 12:25:41 +0200

    refactor home show image by size

--- src/app/(home)/homePage.tsx
+++ src/app/(home)/homePage.tsx
@@ -1,3 +1,5 @@
+'use client'
+import useWindowSize from '@/hooks/useWindowSize'
 import classNames from 'classnames'
 import Image from 'next/image'
 import type { ReactElement } from 'react'
@@ -11,36 +13,57 @@ import leftWingIcon from '../../../public/images/icons/left_wing.svg'
 import rightWingIcon from '../../../public/images/icons/right_wing.svg'
 import styles from './home.module.scss'
 
+const BgPortraitMobileSm = () => <Image
+	fill
+	className={classNames(styles.bgPortrait, styles.bgPortraitMobileSm)}
+	src={bgPortraitMobileSm}
+	alt='Background image Veronika Brook'
+	priority
+	loading='eager'
+	placeholder="blur"
+/>
+
+const BgPortraitMobileXl = () => <Image
+	fill
+	className={classNames(styles.bgPortrait, styles.bgPortraitMobileXl)}
+	src={bgPortraitMobileXl}
+	alt='Background image Veronika Brook'
+	priority
+	loading='eager'
+	placeholder="blur"
+/>
+
+const BgPortraitMobileMd = () => 	<Image
+	className={classNames(styles.bgPortrait, styles.bgPortraitMobileMd)}
+	src={bgPortraitMobileMd}
+	alt='Background image Veronika Brook'
+	loading="eager"
+	placeholder="blur"
+/>
+
 const HomePage = (): ReactElement => {
+	const {screenWidth, screenHeight} = useWindowSize()
+	console.log("screenWidth:", screenWidth)
+	console.log("screenHeight:", screenHeight)
+	
 	return (
-		<div className={styles.root}>
+		<div className={styles.main}>
 			<Image
 				fill
 				className={styles.bgOfPortrait}
 				src={bgOfPortrait}
 				alt='Background home page color'
 				priority
+				loading='eager'
 				placeholder="blur"
 			/>
 			<div className={styles.bgImageWrapper}>
-				<Image
-					fill
-					className={classNames(styles.bgPortrait, styles.bgPortraitMobileSm)}
-					src={bgPortraitMobileSm}
-					alt='Background image Veronika Brook'
-					priority
-					loading='eager'
-					placeholder="blur"
-				/>
-				<Image
-					fill
-					className={classNames(styles.bgPortrait, styles.bgPortraitMobileXl)}
-					src={bgPortraitMobileXl}
-					alt='Background image Veronika Brook'
-					priority
-					loading='eager'
-					placeholder="blur"
-				/>
+				{screenWidth < 1199 && screenWidth <= 330 && BgPortraitMobileSm()}
+				{screenWidth < 1199 && screenHeight < 858 && BgPortraitMobileSm()}
+				{screenWidth < 1199 && screenWidth <= 784 && BgPortraitMobileXl()}
+				{screenWidth < 1199 && screenHeight < 858 && BgPortraitMobileXl()}
+				{screenWidth < 1199 && screenWidth >= 398 && screenWidth <= 784 && BgPortraitMobileMd()}
+				
 				<Image
 					className={classNames(styles.bgPortrait, styles.bgPortraitMobileMd)}
 					src={bgPortraitMobileMd}
@@ -48,6 +71,8 @@ const HomePage = (): ReactElement => {
 					loading="eager"
 					placeholder="blur"
 				/>
+				{/* (min-width: 784px) and (max-width: 1199px) display: block; object-fit: cover; object-position: center;*/}
+				{/* (min-height: 858px) display: none; */}
 				<Image
 					fill
 					className={classNames(styles.bgPortrait, styles.bgPortraitMobileLg)}
@@ -57,6 +82,8 @@ const HomePage = (): ReactElement => {
 					loading='eager'
 					placeholder="blur"
 				/>
+				{/* display: block; min-width: 1199px */}
+				{/* (min-height: 858px) display: none; */}
 				<Image
 					className={classNames(styles.bgPortrait, styles.bgPortraitDesktop)}
 					src={bgPortraitDesktop}
@@ -66,12 +93,15 @@ const HomePage = (): ReactElement => {
 					placeholder="blur"
 				/>
 			</div>
+			{/* (min-height: 858px) top: 60vh; */}
+			{/* @media (min-width: 1199px) eft: 5vw; top: 10vh; */}
 			<div className={styles.textWrapper}>
 				<div className={styles.innerTextWrapper}>
 					<div className={styles.textJustifyWrapper}>
 						<div className={styles.line1And2And3Wrapper}>
 							<h1 className={styles.line1}>OPERA SINGER</h1>
 							<div className={styles.line2Wrapper}>
+								{/* top: -2rem; max-width: 574px */}
 								<div className={styles.line2WrapperAbsolute}>
 									<Image
 										src={leftWingIcon}
